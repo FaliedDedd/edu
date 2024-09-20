a084aeaa3dc5743d10f63b6c882cb93c6e53bb2b
Letov <149243666+FaliedDedd@users.noreply.github.com>
2024-09-20 11:44:02 +0300

    Update MainActivity.kt

--- app/src/main/java/org/thefproject/fedu/MainActivity.kt
+++ app/src/main/java/org/thefproject/fedu/MainActivity.kt
@@ -1,3 +1,9 @@
+
+
+// Copyrigt The F Project 2024
+
+
+
 package org.thefproject.fedu
 
 import android.content.Intent
@@ -7,23 +13,42 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.airbnb.lottie.LottieAnimationView
+import com.airbnb.lottie.LottieDrawable
+import org.thefproject.fedu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        lateinit var binding: ActivityMainBinding
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
+val LottieAnimationView = findViewById<View>(R.id.LottiAnim)
         val loginin = findViewById<View>(R.id.loginin)
 
         loginin.setOnClickListener {
             val g = Intent(this, profile::class.java)
             startActivity(g)
+binding.apply {
+LottiAnim.setMinProgress(0.0f)
+    LottiAnim.setMaxProgress(1.0f)
+    LottiAnim.repeatCount = 10
+    LottiAnim.repeatMode = LottieDrawable.RESTART
+    LottiAnim.playAnimation()
 
+
+}
         }
     }
 }
