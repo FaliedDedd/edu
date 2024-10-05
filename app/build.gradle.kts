10cd3ef7af21970dafca23b120833f88f364177c
Letov <149243666+FaliedDedd@users.noreply.github.com>
2024-10-05 12:31:07 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,30 +2,32 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
 }
-// Не рекомендую как-либо изменять файл gradle 
-// Copyright The F Project 2024
+
 android {
     namespace = "org.thefproject.fedu"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "org.thefproject.fedu"
-        minSdk = 16
-        targetSdk = 34
-        versionCode = 10
-        versionName = "0.4a"
+        minSdk = 22
+        targetSdk = 33
+        versionCode = 160
+        versionName = "0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
+            multiDexEnabled = true
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("debug")
         }
+
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
@@ -37,17 +39,25 @@ android {
 
     buildFeatures {
         viewBinding = true
+      buildConfig = true
+
+       // compose = true
     }
 }
 
 
 dependencies {
+
+// implementation 'com.android.support:multidex:1.0.3'
+    implementation("com.android.support:multidex:1.0.3")
     implementation ("com.airbnb.android:lottie:6.5.2")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.runner)
+    implementation(libs.androidx.espresso.core)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
